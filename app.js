bf695e1041cca544bc4135e75182a14a836e6bc6
Serena Seyler <sseyler0119@gmail.com>
2024-02-26 12:52:25 -0800

    update route

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.get('/', (req, res) => {
+  res.send('Jobs API');
+});
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
